ad88ec5f9163b75a1165a121910336596a3c7bb5
liuyaanng <1757465118@qq.com>
2021-03-08 12:07:21 +0800

    add 动态规划3法

--- 338.counting-bits.cpp
+++ 338.counting-bits.cpp
@@ -5,22 +5,61 @@
  */
 
 // @lc code=start
+// 最低有效位
+// class Solution {
+// public:
+//     vector<int> countBits(int num) {
+// 			vector<int> result(num + 1);
+// 			int highbit = 0;
+// 			for(int x=1; x <= num; x++){
+// 				result[x] = result[x>>1] + (x & 1);
+// 			}
+// 			return result;
+//     }
+// };
+// 最低设置位
 class Solution {
 public:
-	int count_ones(int x){
-		int count = 0;
-		while(x > 0){
-			x &= (x - 1);
-			count++;
-		}
-		return count;
-	}
     vector<int> countBits(int num) {
 			vector<int> result(num + 1);
-			for(int x=0; x <= num; x++){
-				result[x] = count_ones(x);
+			int highbit = 0;
+			for(int x=1; x <= num; x++){
+				result[x] = result[x&(x-1)] +1;
 			}
 			return result;
     }
 };
+// 最高有效位
+// class Solution {
+// public:
+//     vector<int> countBits(int num) {
+// 			vector<int> result(num + 1);
+// 			int highbit = 0;
+// 			for(int x=1; x <= num; x++){
+// 				if((x & (x - 1)) == 0){
+// 					highbit = x;
+// 				}
+// 				result[x] = result[x - highbit] + 1;
+// 			}
+// 			return result;
+//     }
+// };
+// // class Solution {
+// public:
+// 	int count_ones(int x){
+// 		int count = 0;
+// 		while(x){
+// 			x &= (x - 1);
+// 			count++;
+// 		}
+// 		return count;
+// 	}
+//     vector<int> countBits(int num) {
+// 			vector<int> result(num + 1);
+// 			for(int x=0; x <= num; x++){
+// 				result[x] = count_ones(x);
+// 			}
+// 			return result;
+//     }
+// };
 // @lc code=end
