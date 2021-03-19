4ec30bda59de4854da4f8335d520215d0988b7ae
liuyaanng <1757465118@qq.com>
2021-03-19 12:10:56 +0800

    add chaining for 706

--- 706.design-hashmap.cpp
+++ 706.design-hashmap.cpp
@@ -5,30 +5,95 @@
  */
 
 // @lc code=start
+struct MyListNode{
+	int key; 
+	int val;
+	MyListNode* next;
+	MyListNode() : key(-1), val(-1), next(nullptr) {}
+	MyListNode(int _key, int _val) : key(_key), val(_val), next(nullptr) {}
+};
 class MyHashMap {
 public:
     /** Initialize your data structure here. */
     MyHashMap() {
-			const int N = 1000001;
-			data = vector<int>(N, -1);
+			data.resize(N);
     }
-    
+		auto getHashValue(int key){
+			return key % N;
+		}
+
     /** value will always be non-negative. */
     void put(int key, int value) {
-			data[key] = value;
+			auto hashkey = getHashValue(key);
+			auto& head = data[hashkey];
+			if(head==nullptr){
+				head = new MyListNode(key, value);
+				return;
+			}
+			auto p = head;
+			auto tail = p;
+			// add value which has been added
+			while(p != nullptr){
+				if(p->key == key){
+					p->val = value;
+					return;
+				}
+				tail = p;
+				p = p->next;
+			}
+			// add new value
+			tail->next = new MyListNode(key, value);
     }
     
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     int get(int key) {
-			return data[key];
+			auto hashkey = getHashValue(key);
+			auto& head = data[hashkey];
+			if(head==nullptr){
+				return -1;
+			}
+			auto p = head;
+			while(p != nullptr){
+				if(p->key == key){
+					return p->val;
+				}
+				p = p->next;
+			}
+			return -1;
     }
     
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     void remove(int key) {
-			data[key] = -1;
+			auto hashkey = getHashValue(key);
+			auto& head = data[hashkey];
+			if(head==nullptr){
+				return;
+			}
+			auto p = head;
+			MyListNode* prev = nullptr;
+			while(p != nullptr){
+				if(p->key == key){
+					// remove head
+					if(prev == nullptr){
+						auto dummy = head;
+						head = head->next; // nullptr
+						dummy->next = nullptr;
+						delete dummy;
+					}
+					else{
+						prev->next = p->next;
+						p->next = nullptr;
+						delete p;
+					}
+					return;
+				}
+				prev = p;
+				p = p->next;
+			}
     }
 private:
-		vector<int> data;
+		const int N = 1001;
+		vector<MyListNode*> data;
 };
 
 /**
