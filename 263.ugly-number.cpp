3da4013b24d3f7031d4881fd751c7f81e2940703
liuyaanng <1757465118@qq.com>
2021-04-10 16:14:29 +0800

    154 263

---
+++ 263.ugly-number.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=263 lang=cpp
+ *
+ * [263] 丑数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isUgly(int n) {
+			if(n <= 0) return false;
+			vector<int> factors{2,3,5};
+			for(int factor : factors){
+				while(n % factor == 0){
+					n /= factor;
+				}
+			}
+			return n == 1;
+    }
+};
+// @lc code=end
