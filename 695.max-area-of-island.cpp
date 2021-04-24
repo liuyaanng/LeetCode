3f97bce333a35a1d9668a543a341d9ac5f456aaf
liuyaanng <1757465118@qq.com>
2021-04-24 23:09:08 +0800

    695 add two dfs solution

--- 695.max-area-of-island.cpp
+++ 695.max-area-of-island.cpp
@@ -1,7 +1,7 @@
 /*
  * @lc app=leetcode.cn id=695 lang=cpp
  *
- * <0;32;13M[695] 岛屿的最大面积
+ * [695] 岛屿的最大面积
  */
 
 // @lc code=start
@@ -9,34 +9,20 @@ class Solution {
 public:
 		vector<int> direction{-1, 0, 1, 0, -1};
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-			int m = grid.size(), n = m ? grid[0].size() : 0, area = 0, local_area = 0;
-			for(int i = 0; i < m; ++i){
-				for(int j = 0; j < n; ++j){
-					if(grid[i][j] == 1){
-						local_area = 1;
-						//防止重复寻找
-						grid[i][j] = 0;
-						stack<pair<int, int>> island;
-						island.push({i, j});
-
-						while(!island.empty()){
-							auto [r,c] = island.top();
-							island.pop();
-							for(int k = 0; k < 4; ++k){
-								//分别向上下左右移动, 若存在岛屿,将其入栈, 即该处也可向其他方向移动来寻找. 
-								int x = r + direction[k], y = c + direction[k + 1];
-								if(x >= 0 && x < m && y >=0 && y < n && grid[x][y] == 1){
-									grid[x][y] = 0;
-									++local_area;
-									island.push({x, y});
-								}
-							}
-							area = max(area, local_area);
-						}
-					}
+			if(grid.empty() || grid[0].empty()) return 0;
+			int max_area = 0;
+			for (int i = 0; i < grid.size(); ++i) {
+				for (int j = 0; j < grid[0].size(); ++j) {
+						max_area = max(max_area, dfs(grid, i, j));
 				}
 			}
-			return area;
+			return max_area;
     }
+
+		int dfs(vector<vector<int>>& grid, int r, int c){
+			if(r < 0 || r >= grid.size() || c < 0 || c >= grid[0].size() || grid[r][c] == 0) return 0;
+			grid[r][c] = 0; 
+			return 1 + dfs(grid, r - 1, c) + dfs(grid, r + 1, c) + dfs(grid, r, c - 1) + dfs(grid, r, c + 1);
+		}
 };
 // @lc code=end
